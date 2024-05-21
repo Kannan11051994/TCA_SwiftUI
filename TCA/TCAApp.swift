09bfa669321b3e10634f58a80cff36f7709803e2
Kanna <kanna5846@gmail.com>
2024-05-21 10:25:24 +0530

    Initial Commit

---
+++ TCA/TCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCAApp.swift
+//  TCA
+//
+//  Created by Kanna on 21/05/24.
+//
+
+import SwiftUI
+
+@main
+struct TCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
